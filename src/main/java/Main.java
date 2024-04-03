1d0c181f22674d9564afeef4b26f8adfad3f1b1a
Fransheska <fransheskavalenzuela@gmail.com>
2024-04-02 23:49:42 -0300

    Merge branch 'Nicolás'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,4 +3,10 @@ public class Main {
         System.out.println("Fransheska Valenzuela :)");
         System.out.println("Ignacio Valdebenito :");
     }
+
+    public static class HelloWorld {
+        public static void main(String[] args) {
+            System.out.print("Hello world Nicolás Sandoval");
+        }
+    }
 }
